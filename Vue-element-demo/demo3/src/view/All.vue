ad3ecf4a6f24297b0489da57448d4130a0931b51
Your Name <1758555877@qq.com>
2023-03-16 20:52:48 +0800

    修改import引入组件名

--- Vue-element-demo/demo3/src/view/All.vue
+++ Vue-element-demo/demo3/src/view/All.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <User />
-    <Post />
+    <Child />
+    <part />
     <Comment />
   </div>
 </template>
   
 <script>
-import User from "../components/Child.vue";
-import Post from "../components/part.vue";
+import Child from "../components/Child.vue";
+import part from "../components/part.vue";
 import Comment from "../components/Comment.vue";
 
 export default {
   name: "AppVue",
   components: {
-    User,
-    Post,
+    Child,
+    part,
     Comment,
   },
 };
