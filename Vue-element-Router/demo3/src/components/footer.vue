c672bb7706b232b80516604588c7a1928d671284
Your Name <1758555877@qq.com>
2023-03-24 16:41:46 +0800

    学习命名视图

---
+++ Vue-element-Router/demo3/src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+<!-- 在 HTML 中定义命名视图 -->
+<div id="app">
+  <router-view name="header"></router-view>
+  <router-view></router-view>
+  <router-view name="footer"></router-view>
+</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+       name:"FotterApp", 
+    }
+</script>
+
+<style >
+
+</style>
--- Vue-element-Router/demo3/src/router/index.js
+++ Vue-element-Router/demo3/src/router/index.js
@@ -3,20 +3,20 @@ import Router from 'vue-router'
 const Home = () => import('@/components/Home.vue')
 
 
-Router.beforeEach((to, from, next) => {
-    // 在此处添加您的验证逻辑或其他操作
+// Router.beforeEach((to, from, next) => {
+//     // 在此处添加您的验证逻辑或其他操作
 
-    // 如果用户已经登录，则继续导航
-    if (isLoggedIn()) {
-        next();
-    } else {
-        // 否则，将用户重定向到登录页面
-        next('/login');
-    }
-});
-function isLoggedIn() {
-    return localStorage.getItem('token');
-}
+//     // 如果用户已经登录，则继续导航
+//     if (isLoggedIn()) {
+//         next();
+//     } else {
+//         // 否则，将用户重定向到登录页面
+//         next('/login');
+//     }
+// });
+// function isLoggedIn() {
+//     return localStorage.getItem('token');
+// }
 Vue.prototype.$afterEach = function (to, from) {
     // 在每个路由切换后执行的代码
     // 这里可以写一些统计、日志等通用的操作
@@ -46,9 +46,15 @@ export default new Router({
             name: 'Home',
             component: Home,
         },
-
+        {
+            path: '/footer',
+            components: {
+              default: Home, // 默认组件
+              footer: () => import('../components/footer.vue'), // 名为 "footer" 的命名视图
+            }
+          },
     ],
-    afterEach: vm.$afterEach,
+    // afterEach: vm.$afterEach,
 })
 
 
