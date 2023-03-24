33c57fe9b888c3e2b0e1d8d7943e9bb2b57914d9
Your Name <1758555877@qq.com>
2023-03-24 16:07:54 +0800

    学习vue全局前置守卫

--- Vue-element-Router/demo3/src/router/index.js
+++ Vue-element-Router/demo3/src/router/index.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 const Home = () => import('@/components/Home.vue')
+
+
+Router.beforeEach((to, from, next) => {
+    // 在此处添加您的验证逻辑或其他操作
+  
+    // 如果用户已经登录，则继续导航
+    if (isLoggedIn()) {
+      next();
+    } else {
+      // 否则，将用户重定向到登录页面
+      next('/login');
+    }
+  });
+  function isLoggedIn() {
+    return localStorage.getItem('token');
+  }
 export default new Router({
     linkActiveClass: 'isactive',
      mode: 'history',
