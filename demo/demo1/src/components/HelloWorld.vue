07177c86cd06461193364561604fa4c1b76fbcb7
Your Name <1758555877@qq.com>
2023-03-28 17:24:34 +0800

    学习计算属性

---
+++ demo/demo1/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <div id="example">
+      <p>Original message: "{{ message }}"</p>
+      <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    </div>
+    <hr />
+    <div>
+      <p>Reversed message: "{{ reversedMexssage() }}"</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "Hello",
+    };
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 vm 实例
+      return this.message.split("").reverse().join("");
+    },
+  },
+  methods: {
+    reversedMexssage () {
+      return this.message.split("").reverse().join("");
+    },
+  },
+};
+</script>
