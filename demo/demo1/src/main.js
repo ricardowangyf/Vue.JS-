22274e960c817d6be420ce26dccec18ece0c69de
Your Name <1758555877@qq.com>
2023-03-30 17:30:06 +0800

    学习vue全局注册

--- demo/demo1/src/main.js
+++ demo/demo1/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 Vue.config.productionTip = false
 
+Vue.component('my-component', {
+  // 组件选项
+})
+
+
 new Vue({
   router,
   render: h => h(App),
